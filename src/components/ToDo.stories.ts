c56eb5010e2ca06e26cab6f3b1737d55988ccae0
Portero <eportero421@gmail.com>
2024-05-24 14:31:21 -0300

    chore: New course with storybook

---
+++ src/components/ToDo.stories.ts
@@ -0,0 +1,40 @@
+import { Meta } from "@storybook/react";
+import ToDo from "./ToDo";
+
+const meta = {
+
+    title: 'Components/ToDo',
+    component: ToDo,
+
+    args: {
+
+        id: "1",
+        todo: "Hola Mundo",
+
+    }
+
+} satisfies Meta<typeof ToDo>
+
+export default meta;
+
+export const Default = {}
+
+export const Story2 = {
+
+    args: {
+
+        todo: 'Hola Storybook'
+
+    }
+
+}
+
+export const Completed = {
+
+    args: {
+        
+        isCompleted: true,
+
+    }
+
+}
---
+++ src/components/ToDo.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import styles from './ToDo.module.css'
+
+interface TodoProps {
+
+    id: string;
+    todo: string;
+    isCompleted?: boolean
+
+}
+
+const ToDo = ({id, todo, isCompleted}: TodoProps) => {
+
+  const [completed, setCompleted] = useState(isCompleted)
+
+  return (
+    
+    <div>
+        <input type="checkbox" 
+        id={id} 
+        onChange={(e) => setCompleted(e.target.checked)} 
+        checked={completed}
+        />
+
+        <span className={completed ? styles.completed : ''}>{todo}</span>
+        
+    </div>
+
+  )
+}
+
+export default ToDo
